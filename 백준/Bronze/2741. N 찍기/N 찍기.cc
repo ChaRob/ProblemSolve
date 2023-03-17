975c7ba087ee17ecc1d96c0344d9f7744df59c1e
ChaRob <122436538+ChaRob@users.noreply.github.com>
2023-03-17 13:29:19 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << i << "\n";
+	}
+	return 0;
+}
