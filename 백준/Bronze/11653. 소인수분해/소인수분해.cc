6f5f27edfbc7f140ea286c16d6986d099823f20b
ChaRob <122436538+ChaRob@users.noreply.github.com>
2023-03-27 23:27:38 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,28 +1,14 @@
 #include<iostream>
-#include<math.h>
-#include<memory.h>
 using namespace std;
-bool isprime[10000001];
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    int n, i, j;
-    memset(isprime, true, sizeof(isprime));
-    isprime[0] = false; isprime[1] = false;
-    for (i = 2; i < int(sqrt(10000000)) + 1; i++) {
-        if (isprime[i]) {
-            j = 2;
-            while (i * j <= 10000000) {
-                isprime[i * j] = false;
-                j++;
-            }
-        }
-    }
+    int n, i;
     cin >>  n;
     i = 2;
     while (n != 1) {
-        if (isprime[i] && n % i == 0) {
+        if (n % i == 0) {
             n /= i;
             cout << i << "\n";
         }
