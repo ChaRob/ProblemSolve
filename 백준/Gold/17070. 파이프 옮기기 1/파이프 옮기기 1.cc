6ac5f6f20aec2a6c78b910611f11ebd0613185b2
ChaRob <122436538+ChaRob@users.noreply.github.com>
2023-04-06 22:15:27 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,32 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int house[16][16];
-int dp[16][16];
+int house[32][32];
+long long dp[32][32][3];
 
-int move(int x, int y, int dir, int n) {
-	int tmp = 0;
+long long move(int x, int y, int dir, int n) {
+	long long tmp = 0;
 
 	if (x == n - 1 && y == n - 1) {
-		dp[x][y] = 1;
-		return dp[x][y];
+		dp[x][y][dir] = 1;
+		return dp[x][y][dir];
 	}
 
-	/*if (dp[x][y] != -1) {
-		return dp[x][y];
-	}*/
+	if (dp[x][y][dir] != -1) {
+		return dp[x][y][dir];
+	}
 
 	if (dir <= 1 && y + 1 < n && house[x][y + 1] != 1) {
 		tmp += move(x, y + 1, 0, n);
 	}
-	if (dir >= 1 && x + 1 < n && house[x + 1][y] != 1) {
-		tmp += move(x + 1, y, 2, n);
-	}
 	if (x + 1 < n && y + 1 < n && house[x][y + 1] != 1 && house[x + 1][y] != 1 && house[x + 1][y + 1] != 1) {
 		tmp += move(x + 1, y + 1, 1, n);
 	}
-	dp[x][y] = tmp;
-	return dp[x][y];
+	if (dir >= 1 && x + 1 < n && house[x + 1][y] != 1) {
+		tmp += move(x + 1, y, 2, n);
+	}
+	dp[x][y][dir] = tmp;
+	return tmp;
 }
 
 int main()
@@ -34,17 +34,19 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	int n, res;
+	int n;
 	cin >> n;
 	for (size_t i = 0; i < n; i++)
 	{
 		for (size_t j = 0; j < n; j++)
 		{
 			cin >> house[i][j];
-			dp[i][j] = -1;
+			for (size_t k = 0; k < 3; k++)
+			{
+				dp[i][j][k] = -1;
+			}
 		}
 	}
-	res = move(0, 1, 0, n);
-	cout << res;
+	cout << move(0, 1, 0, n);
 	return 0;
 }
