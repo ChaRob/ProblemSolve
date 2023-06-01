e56292ad0394cc73858573ef7390d776f3ba3884
ChaRob <122436538+ChaRob@users.noreply.github.com>
2023-06-01 14:05:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 196 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    int* A = new int[n];
+    vector<int> arr = {-1000000001};
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+        if (arr[arr.size() - 1] < A[i]) {
+            arr.push_back(A[i]);
+        }
+        else {
+            int mid, left = 0, right = arr.size();
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (arr[mid] < A[i]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            arr[left] = A[i];
+        }
+    }
+
+    cout << arr.size() - 1;
+
+    delete[] A;
+    return 0;
+}
