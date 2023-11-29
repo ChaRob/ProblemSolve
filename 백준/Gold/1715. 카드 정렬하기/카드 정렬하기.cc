c76c4b0bdb493a926081bc9c9740976d17bb057b
ChaRob <122436538+ChaRob@users.noreply.github.com>
2023-11-29 10:08:14 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 24 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    priority_queue<ll> heapque;
+    for (int i = 0; i < n; i++)
+    {
+        ll tmp;
+        cin >> tmp;
+        heapque.push(-tmp);
+    }
+    ll res = 0;
+    while (heapque.size() != 1) {
+        ll a, b;
+        a = heapque.top();
+        heapque.pop();
+        b = heapque.top();
+        heapque.pop();
+        heapque.push(a + b);
+        res += a + b;
+    }
+    cout << -(res);
+    return 0;
+}
