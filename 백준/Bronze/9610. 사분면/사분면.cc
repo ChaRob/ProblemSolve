33dced2719b7a0ccae4e41f1fff81213caf6172c
ChaRob <122436538+ChaRob@users.noreply.github.com>
2023-12-31 10:41:48 +0900

    [Bronze III] Title: 사분면, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n,	q1 = 0, q2 = 0, q3 = 0, q4 = 0, axis = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		if (x == 0 || y == 0) {
+			axis++;
+		}
+		else if (x > 0 && y > 0) {
+			q1++;
+		}
+		else if (x < 0 && y > 0) {
+			q2++;
+		}
+		else if (x < 0 && y < 0) {
+			q3++;
+		}
+		else {
+			q4++;
+		}
+	}
+	cout << "Q1: " << q1 << "\n";
+	cout << "Q2: " << q2 << "\n";
+	cout << "Q3: " << q3 << "\n";
+	cout << "Q4: " << q4 << "\n";
+	cout << "AXIS: " << axis;
+	return 0;
+}
