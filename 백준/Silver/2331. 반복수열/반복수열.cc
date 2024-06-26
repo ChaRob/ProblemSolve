903bf8984382672ebcc4c06c00b7b79d58c8c4c3
ChaRob <122436538+ChaRob@users.noreply.github.com>
2024-06-26 09:11:04 +0900

    [Silver IV] Title: 반복수열, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.cc
+++ 백준/Silver/2331. 반복수열/반복수열.cc
@@ -1,74 +1,34 @@
 #include <iostream>
-#include <string>
+#include <unordered_map>
 #include <math.h>
-#include <vector>
-#include <algorithm>
 using namespace std;
-
-struct Info
-{
-	string num;
-	bool repeat;
-};
 
 int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	string A;
-	int P;
-	cin >> A >> P;
-	vector<Info> vec = { Info{A,false} };
-	int check = 0;
-	Info latest{"",false};
+	unordered_map<int, int> nums;
+	int n, p;
+	cin >> n >> p;
+	nums[n]++;
 	while (true)
 	{
-		int sum = 0;
-		latest = (check == 1)? latest : vec.back();
-		for (int i = 0; i < latest.num.length(); i++)
+		int sum = 0, i = 1;
+		while (n > 0)
 		{
-			sum += pow(latest.num[i] - '0', P);
+			sum += (int)pow((n % (int)pow(10, i)), p);
+			n /= 10;
 		}
-		
-		string newNum;
-		int i = 1;
-		while (sum > 0)
-		{
-			char c = (sum % (int)pow(10, i)) + '0';
-			newNum += c;
-			sum /= 10;
-		}
-		reverse(newNum.begin(), newNum.end());
-		check = 0;
-		vector<Info>::iterator iter;
-		for (iter = vec.begin(); iter != vec.end(); iter++)
-		{
-			if (iter->num == newNum) {
-				if (iter->repeat == true) {
-					check = 2;
-				}
-				else {
-					iter->repeat = true;
-					check = 1;
-				}
-				break;
-			}
-		}
-
-		if (check == 2) {
+		if (nums[sum] > 1) {
 			break;
 		}
-		else if (check == 1) {
-			latest = *iter;
-		}
-		else {
-			vec.push_back(Info{newNum,false});
-		}
+		nums[sum]++;
+		n = sum;
 	}
 	int cnt = 0;
-	for (int i = 0; i < vec.size(); i++)
+	for (auto& res : nums)
 	{
-		if (!vec[i].repeat) cnt++;
+		if (res.second == 1) cnt++;
 	}
 	cout << cnt;
 	return 0;
