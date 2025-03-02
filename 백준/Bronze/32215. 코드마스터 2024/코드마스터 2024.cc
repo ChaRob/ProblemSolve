d03fc0c17b51516a2f66c1a3f9e8727b121cb52f
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-03-02 23:49:47 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n, m, k;
+	cin >> n >> m >> k;
+	cout << (k + 1) * m;
+	return 0;
+}
