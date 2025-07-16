d39da7563695103cad911315fcf78620d92b06d2
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-07-17 08:32:24 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	if (n == 2) cout << n + 1;
+	else cout << n;
+	return 0;
+}
