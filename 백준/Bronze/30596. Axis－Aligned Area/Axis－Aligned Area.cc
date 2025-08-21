53ceecaff33316ddcd08dc87399a529722522867
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-08-21 09:05:51 +0900

    [Bronze III] Title: Axis-Aligned Area, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a * c;
+	return 0;
+}
