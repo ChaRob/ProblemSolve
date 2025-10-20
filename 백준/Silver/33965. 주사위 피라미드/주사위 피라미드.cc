79ba36b496275a64419abf297c5451dc2d05fd87
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-10-20 09:38:10 +0900

    [Silver V] Title: 주사위 피라미드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/33965. 주사위 피라미드/주사위 피라미드.cc
+++ 백준/Silver/33965. 주사위 피라미드/주사위 피라미드.cc
@@ -7,26 +7,6 @@ int main()
 	ios_base::sync_with_stdio(false);
 	int n;
 	cin >> n;
-	int min = 0, max = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		if (i == 1)
-		{
-			min += 15;
-			max += 20;
-		}
-		if (i >= 2)
-		{
-			// side + pillar
-			min += 10 * 2 + 3;
-			max += 18 * 2 + 11;
-		}
-		if (i >= 3)
-		{
-			min += (i - 2) * 8;
-			max += (i - 2) * 27;
-		}
-	}
-	cout << min + max;
+	cout << 35 * (n + 1) * n / 2;
 	return 0;
 }
