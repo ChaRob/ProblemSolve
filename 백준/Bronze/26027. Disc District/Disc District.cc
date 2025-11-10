2a476f6e79b741e1cc85ceedb2f1a70fd7b60d58
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-11-10 09:24:01 +0900

    [Bronze II] Title: Disc District, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26027. Disc District/Disc District.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int r;
+    cin >> r;
+    cout << 1 << " " << r;
+    return 0;
+}
