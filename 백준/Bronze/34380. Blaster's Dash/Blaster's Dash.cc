24958c11dddc2b501495e28b4f007837d5de8619
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-12-02 09:08:04 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int m, v, n, k;
+    cin >> m >> v >> n >> k;
+    cout << 2 * n + 40;
+    return 0;
+}
