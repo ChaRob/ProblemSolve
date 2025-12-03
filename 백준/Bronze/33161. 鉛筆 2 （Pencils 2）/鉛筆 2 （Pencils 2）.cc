79846db96a73c64650d068b6cd888885fe053787
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-12-03 09:21:13 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int a;
+    cin >> a;
+    cout << a / 5;
+    return 0;
+}
