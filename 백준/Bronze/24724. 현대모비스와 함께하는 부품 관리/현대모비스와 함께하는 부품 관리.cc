017c9637723786b3a02d71a389fc54c904f8b1e0
ChaRob <122436538+ChaRob@users.noreply.github.com>
2025-12-14 23:56:24 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int T;
+	cin >> T;
+	for (int t = 1; t <= T; t++)
+	{
+		cout << "Material Management " << t << "\n";
+		int n, a, b;
+		cin >> n >> a >> b;
+		for (int i = 0; i < n; i++)
+		{
+			int u, v;
+			cin >> u >> v;
+		}
+		cout << "Classification ---- End!\n";
+	}
+	return 0;
+}
