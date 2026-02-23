8242aad5d10e2af20613c053263fb5a176d2ed5e
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-02-23 09:54:41 +0900

    [Silver V] Title: 레몬 게임, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/34248. 레몬 게임/레몬 게임.cc
+++ 백준/Silver/34248. 레몬 게임/레몬 게임.cc
@@ -6,31 +6,16 @@ using namespace std;
 int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	int n;
+	int n, ct = 0, co = 0;
 	cin >> n;
-	vector<int> tray;
 	for (int i = 0; i < n; i++)
 	{
-		int lemon, sum = 0;
+		int lemon;
 		cin >> lemon;
-		tray.push_back(lemon);
-		int size = tray.size();
-		while (--size >= 0)
-		{
-			sum += tray[size];
-			if (sum == 3)
-			{
-				for (int i = tray.size() - 1; i >= size; i--)
-				{
-					tray.pop_back();
-				}
-				break;
-			}
-			else if (sum > 3)
-				break;
-		}
+		if (lemon == 1) co++;
+		else ct++;
 	}
-	if (tray.size() > 0)
+	if (co < ct || (co - ct) % 3 != 0)
 		cout << "No";
 	else
 		cout << "Yes";
