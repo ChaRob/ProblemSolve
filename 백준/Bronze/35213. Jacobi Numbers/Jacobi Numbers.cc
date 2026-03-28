94adf16f717509714cb972f12d59fa2a9e0add02
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-03-29 07:53:55 +0900

    [Bronze III] Title: Jacobi Numbers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/35213. Jacobi Numbers/Jacobi Numbers.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	cout << n << "\n";
+	for (int i = 0; i < n; i++)
+		cout << 1 << " ";
+	return 0;
+}
