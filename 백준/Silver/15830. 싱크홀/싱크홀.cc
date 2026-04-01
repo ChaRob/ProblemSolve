15de3b5d88bb142ea0d354fad8e4b2041231e5c5
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-04-02 08:49:57 +0900

    [Silver III] Title: 싱크홀, Time: 0 ms, Memory: 2020 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/15830. 싱크홀/싱크홀.cc
+++ 백준/Silver/15830. 싱크홀/싱크홀.cc
@@ -8,11 +8,11 @@ int main()
     double v, w, d, s = 0;
     int cnt = 0;
     cin >> v >> w >> d;
+    double t = w / v;
     while (true)
     {
-         double t = w / v;
-         v *= 0.8;
          s += 5 * t * t;
+         t *= 1.25;
          if (s >= d)
              break;
          else
