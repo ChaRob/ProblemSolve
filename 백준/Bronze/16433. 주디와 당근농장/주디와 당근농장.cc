03cf00e79d131b702c28e73f8d97dd7a880e895b
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-04-14 08:54:49 +0900

    [Bronze I] Title: 주디와 당근농장, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/16433. 주디와 당근농장/주디와 당근농장.cc
+++ 백준/Bronze/16433. 주디와 당근농장/주디와 당근농장.cc
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <queue>
 using namespace std;
-
-bool field[99][99];
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
 
 int main()
 {
@@ -12,30 +7,11 @@ int main()
     cin.tie(nullptr);
     int n, r, c;
     cin >> n >> r >> c;
-    queue<pair<int, int>> que;
-    que.push({ r - 1, c - 1 });
-    field[r - 1][c - 1] = true;
-    while (!que.empty())
-    {
-        auto [x, y] = que.front();
-        que.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && !field[nx][ny])
-            {
-                if (!field[x][y])
-                    field[nx][ny] = true;
-                que.push({ nx,ny });
-            }
-        }
-    }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (field[i][j] == true)
+            if ((i + j) % 2 == (r + c) % 2)
                 cout << "v";
             else
                 cout << ".";
