ba388cbb4e78f650b5381ca71fff7f1282a7c385
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-05-06 08:37:48 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.82 ms, Memory: 6.52 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> intStrs, int k, int s, int l) {
+    vector<int> answer;
+    for (auto str : intStrs)
+    {
+        string intStr = str.substr(s, l);
+        int num = stoi(intStr);
+        if (num > k)
+            answer.push_back(num);
+    }
+    return answer;
+}
