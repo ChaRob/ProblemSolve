284041c48243ef0a17171f9a492d37da036a0cbf
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-05-09 18:52:22 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 3.92 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer(52, 0);
+    for(char c : my_string)
+    {
+        if (c >= 'a')
+            answer[c - 'a' + 26]++;
+        else
+            answer[c - 'A']++;
+    }
+    return answer;
+}
