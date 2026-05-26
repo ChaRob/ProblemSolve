8719b43fc750042fb884238ab209adcbe5b4eb8f
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-05-26 09:22:00 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for (int i = 0; i <= myString.size() - pat.size(); i++)
+    {
+        bool check = true;
+        for (int j = 0; j < pat.size(); j++)
+        {
+            if (myString[i + j] != pat[j])
+            {
+                check = false;
+                break;
+            }
+        }
+        if (check)
+            answer++;
+    }
+    return answer;
+}
