3ecbb626a9eede3f57744d0209071d587efa3a42
ChaRob <122436538+ChaRob@users.noreply.github.com>
2026-06-04 08:53:35 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    int answer = 0;
+    if (date1[0] < date2[0] ||
+       (date1[0] == date2[0] && date1[1] < date2[1]) ||
+       (date1[0] == date2[0] && date1[1] == date2[1] && date1[2] < date2[2]))
+        answer = 1;
+    return answer;
+}
